0751c0231d5b886af34e18da3d5bf32d44411b7f
Scott Griv <scottgrivner@gmail.com>
2024-05-06 22:21:26 -0400

    Update portal netlify code

--- netlify/functions/fetchMetadata.js
+++ netlify/functions/fetchMetadata.js
@@ -1,8 +1,8 @@
-// Using node-fetch to handle HTTP requests
 const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
-  const url = event.queryStringParameters.url; // URL is passed as a query parameter
+  const url = event.queryStringParameters.url;
+  console.log("Fetching URL:", url); // Log to see what URL is being requested
 
   if (!url) {
     return {
@@ -12,17 +12,24 @@ exports.handler = async function(event, context) {
   }
 
   try {
-    const response = await fetch(url);
+    const apiURL = `https://api.microlink.io/?url=${encodeURIComponent(url)}&meta`;
+    console.log("API Request URL:", apiURL); // Log the full API request URL
+    const response = await fetch(apiURL);
     const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
 
     return {
       statusCode: 200,
-      body: JSON.stringify(data)
+      body: JSON.stringify(data.data)
     };
   } catch (error) {
+    console.error('Fetch error:', error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: 'Failed to fetch data', error })
+      body: JSON.stringify({ message: 'Failed to fetch data', error: error.toString() })
     };
   }
 };
